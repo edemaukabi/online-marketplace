85d4784dd10868128e7ba3789f3a1f81073dc311
edemaukabi <edemaukabi@gmail.com>
2023-05-06 23:07:17 +0100

    add detail page and related items

--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from items.models import Category, Item
+from item.models import Category, Item
 
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
--- puddle/urls.py
+++ puddle/urls.py
@@ -1,11 +1,15 @@
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 from core.views import index
 from core.views import contact
 
 urlpatterns = [
     path('', index, name='index'),
+    path('items/', include('item.urls')),
     path('contact/', contact, name='contact'),
     path('admin/', admin.site.urls),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
